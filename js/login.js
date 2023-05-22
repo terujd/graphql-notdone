8ddacfbb8f024f91ea14e691975abdea0d4e9fc2
terujd <jansson.michael.93@gmail.com>
2023-05-23 02:35:43 +0300

    remove css stuff

--- js/login.js
+++ js/login.js
@@ -1,35 +1,56 @@
-import { loginTitle, loginTitleText, spanElements, generateRandomColor } from "./css.js";
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { appendTextToElement, continuouslyChangeColor } from './css.js';
+const loginButton = document.getElementById("login-button");
+const loginForm = document.getElementById("login-form");
+const colorTitle = document.querySelector("h1");
+const colorTitleText = colorTitle.textContent;
+const errorContainer = document.getElementById("login-error-msg-holder");
+const spanElements = document.querySelectorAll("h1 span");
+colorTitle.innerHTML = "";
+const errorMessage = document.getElementById("login-error-msg");
 
-const client = new ApolloClient({
-    uri: '/graphql',
-    cache: new InMemoryCache()
-  });
-  
+
   loginButton.addEventListener("click", async (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+
+    const credentials = btoa(`${username}:${password}`);
+
     try {
-      const { data } = await client.mutate({
-        mutation: gql`
-          mutation Login($username: String!, $password: String!) {
-            login(username: $username, password: $password) {
-              token
-            }
-          }
-        `,
-        variables: {
-          username,
-          password
-        }
+      const response = await fetch("https://01.gritlab.ax/api/auth/signin", {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${credentials}`,
+        },
       });
-      const token = data.login.token;
-      localStorage.setItem('token', token);
-      alert("You have successfully logged in.");
-      location.reload();
-    } catch (error) {
-      loginErrorMsg.style.opacity = 1;
+
+      if (response.ok) {
+        const data = await response.json();
+        const jwt = data;
+        localStorage.setItem("jwt", jwt);
+        window.location.href = "/profile.html";
+      } else if (response.status === 401) {
+        errorMessage.textContent = "Wrong username or password";
+        errorContainer.style.border = "1px solid red";
+
+      }
     }
+    catch (error) {
+      console.log("Error:", error);
+      errorMessage.textContent = "Something went wrong";
+      errorContainer.style.border = "1px solid red";
+    }
+  })
+appendTextToElement(colorTitleText, colorTitle);
+
+spanElements.forEach((span) => {
+  span.addEventListener("animationiteration", () => {
+    const randomColor = generateRandomColor();
+    span.style.color = randomColor;
   });
-  
+});
+
+
+continuouslyChangeColor("span", 1000); // Call the function to continuously change color every 1 second
+
+continuouslyChangeColor('title', 1000); // Call the function to continuously change color every 1 second
